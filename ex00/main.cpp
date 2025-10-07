db3c3eac30941a87934992db7036410e03e7c08d
fde-jesu <Fernando Lucart>
2025-10-07 07:04:32 +0100

    asd

---
+++ ex00/main.cpp
@@ -0,0 +1,18 @@
+#include "BitcoinExchange.hpp"
+
+int main(int ac, char **av)
+{
+    if (ac != 2)
+        return 1;
+    try
+    {
+        BitcoinExchange btc;
+        btc.analise_doc(av[1]);
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+    
+    return 0;
+}
